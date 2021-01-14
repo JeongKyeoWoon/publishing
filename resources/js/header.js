c9e3b4151f4ef2b4a5d0c66ca34e12c40c98bb3f
JeongKyeoWoon <kwoon012@gmail.com>
2021-01-14 21:36:59 +0900

    first commit

---
+++ resources/js/header.js
@@ -0,0 +1,20 @@
+(function () {
+    const header = document.querySelector("header");
+    header.innerHTML = (`
+        <div class="container">
+            <div class="header">
+                <div class="header_title">
+                    <h1>Anvil Publishing Guide</h1>
+                </div>
+                <nav class="nav">
+                    <ul>
+                        <li><a href="#">About</a></li>
+                        <li><a href="#">HTML5</a></li>
+                        <li><a href="#">CSS3</a></li>
+                        <li><a href="#">JS</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    `);
+})();
